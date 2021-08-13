ba32ede74e9f313cd547edcc368448026368a804
rahul <rkumar.jnvr@gmail.com>
2021-08-13 13:45:28 +0530

    add start script

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ app.use((err, req, res, next) => {
   if (!err.message) err.message = "Oh No, Something Went Wrong!";
   res.status(statusCode).render("error", { err });
 });
-
-app.listen(8000, () => {
-  console.log("server start");
+const port=process.env.PORT||8000
+app.listen(port, () => {
+  console.log(`server start ${port}`);
 });
